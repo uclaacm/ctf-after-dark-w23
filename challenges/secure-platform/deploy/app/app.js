0648b9b91f2b7c4251775f67cf008a240d9db0f8
ssalandary <86086118+ssalandary@users.noreply.github.com>
2023-02-25 17:32:24 -0800

    changed wording to make goal more clear

--- challenges/secure-platform/deploy/app/app.js
+++ challenges/secure-platform/deploy/app/app.js
@@ -21,7 +21,7 @@ app.get("/flag", (req, res) => {
   if(platform === '"INTEGRITY-178B"'){
     res.send(`<p>what a secure OS! of course you can have my flag!! <\p><p>flag{sh0uldv3_us3d_n4v1g4t0r}<\p>`);
   } else {
-    res.send(`<p>Your Operating system isn't secure enough! I only like Operating systems with an EAL of 6 or higher.<\p><p>Check out INTEGRITY-178B, that's a much better platform.<\p>`);
+    res.send(`<p>Your platform isn't secure enough! I only like platforms with an EAL of 6 or higher.<\p><p>Your platform is ${platform}, I'd much rather it be "INTEGRITY-178B".<\p>`);
   }
 });
 
