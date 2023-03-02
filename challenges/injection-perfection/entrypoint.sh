9ea2eb50e5508c91bd8863c779e94d3f98c59309
Benson Liu <bensonhliu@gmail.com>
2023-03-01 19:24:59 -0800

    prevent crashes lolz

--- challenges/injection-perfection/entrypoint.sh
+++ challenges/injection-perfection/entrypoint.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 docker build -t rory/injection-perfection .
-docker run -d -p 10678:8080 rory/injection-perfection
+docker run -d --restart unless-stopped -p 10678:8080 rory/injection-perfection
